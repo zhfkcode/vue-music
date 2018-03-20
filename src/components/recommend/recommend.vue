b6fe04eef482359b89984b5b5c106dee6edca8de
zhlearn <zhlearn@sina.com>
2018-03-20 09:38:51 +0800

    first commit

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="recommend">
+    <div class="recommend-content">
+      <div class="slider-wrapper">
+
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul></ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {getRecommend} from '../../api/recommend'
+import {ERR_OK} from '../../api/config'
+export default {
+
+  data () {
+    return {
+    };
+  },
+
+ created(){
+   this._getRecommend()
+ },
+  methods: {
+  _getRecommend(){
+    getRecommend().then((res)=>{
+      // console.log(res.code)
+      if(res.code === ERR_OK){
+        console.log(res.data.slider)
+      }
+    })
+  }
+  }
+}
+
+</script>
+<style scoped lang='stylus' rel='stylesheet/stylus'>
+</style>
